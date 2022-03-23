f52d58117ef333a49e8f9e140d4b44e8689a0f25
Sahil Raut <91026979+Sahilraut17@users.noreply.github.com>
2022-03-22 20:01:25 -0400

    Add files via upload

---
+++ index.py
@@ -0,0 +1,83 @@
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output, State
+import dash_bootstrap_components as dbc
+
+# must add this line in order for the app to be deployed successfully on Heroku
+from app import server
+from app import app
+# import all pages in the app
+from apps import trends, maps, analysis, home
+
+# building the navigation bar
+# https://github.com/facultyai/dash-bootstrap-components/blob/master/examples/advanced-component-usage/Navbars.py
+dropdown = dbc.DropdownMenu(
+    children=[
+        dbc.DropdownMenuItem("Analysis", href="/analysis"),
+        dbc.DropdownMenuItem("Trends", href="/trends"),
+        dbc.DropdownMenuItem("Map", href="/map"),
+    ],
+    nav = True,
+    in_navbar = True,
+    label = "Explore",
+)
+
+navbar = dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink("Home", href="/home")),
+        dbc.NavItem(dbc.NavLink("Analysis", href="/analysis")),
+        dbc.NavItem(dbc.NavLink("Trends", href="/trends")),
+        dbc.NavItem(dbc.NavLink("Map", href="/map")),
+        dbc.DropdownMenu(
+            children=[
+                dbc.DropdownMenuItem("More pages", header=True),
+                dbc.DropdownMenuItem("Page 2", href="#"),
+                dbc.DropdownMenuItem("Page 3", href="#"),
+            ],
+            nav=True,
+            in_navbar=True,
+            label="More",
+        ),
+    ],
+    brand="Car Accident Severity Analysis Dashboard",
+    brand_href="/home",
+    color="dark",
+    dark=True,
+    className="mb-4",
+)
+
+
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+for i in [2]:
+    app.callback(
+        Output(f"navbar-collapse{i}", "is_open"),
+        [Input(f"navbar-toggler{i}", "n_clicks")],
+        [State(f"navbar-collapse{i}", "is_open")],
+    )(toggle_navbar_collapse)
+
+# embedding the navigation bar
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    navbar,
+    html.Div(id='page-content')
+])
+
+
+@app.callback(Output('page-content', 'children'),
+              [Input('url', 'pathname')])
+def display_page(pathname):
+    if pathname == '/trends':
+        return trends.layout
+    elif pathname == '/map':
+        return maps.layout
+    elif pathname == '/analysis':
+        return analysis.layout
+    else:
+        return home.layout
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
