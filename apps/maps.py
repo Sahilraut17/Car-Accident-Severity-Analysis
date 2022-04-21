ff758b6948957d44669a7e69be1772af7fbfb601
Sahil Raut <91026979+Sahilraut17@users.noreply.github.com>
2022-04-21 03:15:09 -0400

    Add files via upload

--- apps/maps.py
+++ apps/maps.py
@@ -7,7 +7,7 @@ from dash import html
 from dash.dependencies import Input, Output
 from dash import dash_table
 import dash_bootstrap_components as dbc
-
+import plotly.express as px
 from app import app
 
 # needed only if running this as a single page app
@@ -15,114 +15,157 @@ from app import app
 
 #app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-df = pd.read_csv('US_Accidents_Dec20_new.csv')
+data = pd.read_csv(r'US_Accidents_Dec20_new.csv')
 
-# rename columns
-#df.rename(columns={'Intensive Care Unit (ICU)': 'Intensive Care Unit',
-#                   'General Wards MOH report': 'General wards',
- #                  'In Isolation MOH report': 'In Isolation',
-  #                 'Total Completed Isolation MOH report': 'Total completed isolation',
-   #                'Total Hospital Discharged MOH report': 'Total discharged from hospital',
-    #               'Local cases residing in dorms MOH report': 'Local cases residing in dorms',
-     #              'Local cases not residing in doms MOH report': 'Local cases not residing in dorms'},inplace=True)
+data1=data[['ID','Temperature(F)','Wind_Chill(F)','Humidity(%)','Visibility(mi)','Wind_Direction','Wind_Speed(mph)','Precipitation(in)','Civil_Twilight','Nautical_Twilight','Astronomical_Twilight','Weather_Condition']]
+data2=data[['Severity','City','County','State','Timezone','Start_Lat','Start_Lng','End_Lat','End_Lng', 'Start_Time']]
+data_merged = pd.concat([data1, data2], axis=1)
+data_merged['Start_Time'] = pd.to_datetime(data_merged['Start_Time'])
+data_merged['Week_day'] = data_merged['Start_Time'].dt.dayofweek
+data_merged['Month'] = data_merged['Start_Time'].dt.month
+data_merged = data_merged.astype({'Month':'string'})
+data_merged['Year'] = data_merged['Start_Time'].dt.year
 
-# change to app.layout if running as single page app instead
+#World map
+
+Top_states_Accident_count=data_merged.groupby('State')["ID"].count()
+Top_states_Accident_count=Top_states_Accident_count.reset_index(name='Accident_count')
+
+fig_worldmap = px.choropleth(Top_states_Accident_count, locations="State",locationmode="USA-states", color='Accident_count',
+                           color_continuous_scale="OrRd",
+                           scope="usa"
+                          )
+
+#Pie Chart
+
+Top_10_states_accident=Top_states_Accident_count.nlargest(10,'Accident_count')
+
+
+pie_fig = px.pie(Top_10_states_accident, values='Accident_count', names='State', color_discrete_sequence=px.colors.sequential.Sunset,hole=0.5)
+
+#bar Plot
+
+Bar_accident_fig = px.bar(Top_10_states_accident, x='State', y='Accident_count',
+             hover_data=['State', 'Accident_count'], color='Accident_count', color_continuous_scale='OrRd')
+
+#Timezone and State Analysis 
+
+timezone_df = pd.DataFrame(data_merged['Timezone'].value_counts()).reset_index().rename(columns={'index':'Timezone', 'Timezone':'Cases'})
+
+#pie_fig = px.pie(timezone_df, values='Cases', names='Timezone', color_discrete_sequence=px.colors.sequential.Sunset,hole=0.5)
+
+
+#Grouping the States with respect to the count of accidents
+Timezone_states=data_merged.groupby(['Timezone', 'State'])["ID"].count()
+Timezone_states=Timezone_states.reset_index(name='Accident_count')
+Timezone_state_fig = px.sunburst(Timezone_states,path=['Timezone', 'State'], values='Accident_count',  hover_data=['Timezone', 'State','Accident_count' ], color='Accident_count', color_continuous_scale='OrRd')
+
+#City Analysis
+# create a dataframe of city and their corresponding accident cases
+
+#city_df=data_merged.groupby(['Timezone', 'State','City' ])["ID"].value_counts()
+
+city_df = pd.DataFrame(data_merged['City'].value_counts()).reset_index().rename(columns={'index':'City', 'City':'Cases'})
+#city_df_50 = pd.DataFrame(city_df.head(50))
+
+Treemap_city = px.treemap(city_df, path=[px.Constant('USA'),'City'], values='Cases' ,color='Cases', color_continuous_scale='RdBu' ,hover_data=['City', 'Cases'])
+
+
+city_df_2 = pd.DataFrame(data_merged['City'].value_counts()).reset_index().rename(columns={'index':'City', 'City':'Cases'})
+top_10_cities = pd.DataFrame(city_df_2.head(10))
+
+pie_fig_cities = px.pie(top_10_cities, values='Cases', names='City', color_discrete_sequence=px.colors.sequential.RdBu[::-1],hole=0.5)
+
 layout = html.Div([
     dbc.Container([
-        dbc.Row([
-            dbc.Col(html.H1("US Car Accidents at a glance"), className="mb-2")
-        ]),
-        dbc.Row([
-            dbc.Col(html.H6(children='Visualising trends across different states'), className="mb-4")
-        ]),
 
+        
+
+        # Data Overview
         dbc.Row([
             dbc.Col(dbc.Card(html.H3(children='Data Overview',
                                      className="text-center text-light bg-dark"), body=True, color="dark")
                     , className="mb-4")
         ]),
-
-    dcc.RadioItems(
-        id='table_type',
-        options=[{'label': i, 'value': i} for i in ['Condensed table', 'Full table']],
-        value='Condensed table',
-        labelStyle={'display': 'inline-block'}
-    ),
-    dash_table.DataTable(
-        id='datatable',
-        style_table={'overflowX': 'scroll',
-                        'padding': 10},
-        style_header={'backgroundColor': '#25597f', 'color': 'white'},
-        style_cell={
-            'backgroundColor': 'white',
-            'color': 'black',
-            'fontSize': 13,
-            'font-family': 'Nunito Sans'}),
-
-    dbc.Row([
-            dbc.Col(dbc.Card(html.H3(children='Trend of accidents across different states',
-                                     className="text-center text-light bg-dark"), body=True, color="dark")
-                    , className="mt-4 mb-5")
+        dbc.Row([
+            dbc.Col(html.H4(children='In this dataset, we have different attributes like City, State, Timezone and even street for each accident records. Here we will analyze these four features based on the no. of cases for each distinct location.'), className="mb-4")
         ]),
 
-    dcc.Dropdown(
-        id='Weather',
-        options=[
-            {'label': 'Cloudy Weather', 'value': 'Cloudy'},
-            {'label': 'Light Rain', 'value': 'Light Rain'},
-            {'label': 'Mostly Cloudy', 'value': 'Mostly Cloudy'},
-            {'label': 'Partly Cloudy', 'value': 'Partly Cloudy'}
+        #TimeZone Analysis
+        dbc.Row([
+            dbc.Col(dbc.Card(html.H3(children='TimeZone Accident trend Analysis in USA',
+                                     className="text-center text-light bg-dark"), body=True, color="dark")
+            , className="mt-4 mb-4"),
+            
+        ]),
+
+        dbc.Row([
+            
+            dbc.Col(dbc.Card(html.H4(children='Eastern time zone region of US has the highest no. of road accident cases while Mountain time zone region of US has the lowest no. of road accident cases',
+                                     className="text-center text-light bg-dark"), body=True, color="grey")
+            , className="mt-4 mb-4"),
+            dbc.Col(dcc.Graph(figure=Timezone_state_fig)),
+            
         ],
-        value='CB',
-        style={'width': '65%', 'margin-left':'5px'}
+        align="center",
         ),
 
-    dbc.Row([
-        dbc.Col(html.H5(children='Analysis of Top 5 Weather conditions contributing to accidents', className="text-center"),
-                className="mt-4")
-    ]),
 
-    dcc.Graph(id='graph_by_period'),
+        #State wise Analysis
+        dbc.Row([
+            dbc.Col(dbc.Card(html.H3(children='State wise Accident trend in USA',
+                                     className="text-center text-light bg-dark"), body=True, color="dark")
+            , className="mt-4 mb-4"),
+            
+        ]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(figure=fig_worldmap))    
+        ]),
+        dbc.Row([
+            dbc.Col(dbc.Card(html.H3(children='Top 10 States for Accidents',
+                                     className="text-center text-light bg-dark"), body=True, color="dark")
+            , className="mt-4 mb-4"),
+            
+        ]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(figure=Bar_accident_fig)),
+            dbc.Col(dbc.Card(html.H4(children='California(CA) has the most number of accidents followed by Florida(FL) and Texas(TX). It is interesting to see that the number of accidents in California is almost twice the number of accidents in Florida.',
+                                     className="text-center text-light bg-dark"), body=True, color="grey")
+            , className="mt-4 mb-4"),
+            
+        ],
+        align="center",
+        ),
 
-    dbc.Row([
-        dbc.Col(html.H5(children='Statewise accident severity and frequency', className="text-center"),
-                className="mt-4")
-    ]),
+        #City wise Analysis
+        dbc.Row([
+            dbc.Col(dbc.Card(html.H3(children='City wise Accident trend analysis in USA',
+                                     className="text-center text-light bg-dark"), body=True, color="dark")
+            , className="mt-4 mb-4"),
+            
+        ]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(figure=Treemap_city))    
+        ]),
+        dbc.Row([
+            dbc.Col(dbc.Card(html.H3(children='Top 10 States for Accidents',
+                                     className="text-center text-light bg-dark"), body=True, color="dark")
+            , className="mt-4 mb-4"),
+            
+        ]),
+        dbc.Row([
+            
+            dbc.Col(dbc.Card(html.H4(children='Miami is the city with highest no. of road accidents 17.2% followed by L:os Angeles with 14.4%. These Cities. The Top 10 cities constitute a large amount of accident cases. Givernment can focus more on these states to build stricter laws and actions to prevent accidents.',
+                                     className="text-center text-light bg-dark"), body=True, color="grey")
+            , className="mt-4 mb-4"),
+            dbc.Col(dcc.Graph(figure=pie_fig_cities)),
+            
+        ],
+        align="center",
+        ),
 
-    dcc.Graph(id='choropleth_us'),
-
-    # dbc.Row([
-    #     dbc.Col(html.H5(children='Breakdown of cases: local vs imported', className="text-center"),
-    #             width=6, className="mt-4"),
-    #     dbc.Col(html.H5(children='Breakdown of cases: whether residing in dorms', className="text-center"), width=6,
-    #             className="mt-4"),
-    #     ]),
-    # dbc.Row([
-    #     dbc.Col(
-    #         dcc.Graph(id='local and imported'), width=6),
-    #     dbc.Col(dcc.Graph(id='dorms'), width=6)
-    # ]),
-
-    # dcc.Dropdown(
-    #     id='choose_hospital_situation',
-    #     options=[
-    #         {'label': 'ICU', 'value': 'Intensive Care Unit'},
-    #         {'label': 'General wards', 'value': 'General wards'},
-    #         {'label': 'Isolation', 'value': 'In Isolation'},
-    #         {'label': 'Total completed isolation', 'value': 'Total completed isolation'},
-    #         {'label': 'Total discharged from hospital', 'value': 'Total discharged from hospital'},
-    #     ],
-    #     value=['Intensive Care Unit', 'General wards'],
-    #     multi=True,
-    #     style={'width': '70%', 'margin-left':'5px'}
-    #     ),
-
-    # dbc.Row([
-    #     dbc.Col(html.H5(children='Situation in local hospitals', className="text-center"),
-    #             className="mt-4")
-    # ]),
-
-    # dcc.Graph(id='situation_graph_by_period')
+        
+        
     ])
 
 ])
